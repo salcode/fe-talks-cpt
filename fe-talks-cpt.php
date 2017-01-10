4f56641a0dee1c10bf12d8fe2e9c4c7d33301554
Sal Ferrarello <sal@ironcodestudio.com>
2017-01-09 21:37:57 -0500

    Initial commit add base plugin file

---
+++ fe-talks-cpt.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Iron Code Talks Custom Post Type
+ * Plugin URI: https://github.com/salcode/fe-talks-cpt
+ * Description: Create Custom Post Type for Talks
+ * Version: 0.1.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: fe-talks-cpt
+ * Domain Path: /languages
+ *
+ * @package fe-talks-cpt
+ */
